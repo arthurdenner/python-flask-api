e243b47c02ae95f4e33cbacb1ecd59d73cf9e1ca
Arthur Denner <arthurdenner7@gmail.com>
2020-07-22 21:11:03 +0200

    feat: add authentication and authorization with JWT

--- resources/routes.py
+++ resources/routes.py
@@ -1,6 +1,10 @@
+from resources.auth import SignupApi, LoginApi
 from resources.movie import MoviesApi, MovieApi
 
 
 def initialize_routes(api):
     api.add_resource(MoviesApi, '/api/movies')
     api.add_resource(MovieApi, '/api/movies/<id>')
+
+    api.add_resource(SignupApi, '/api/auth/signup')
+    api.add_resource(LoginApi, '/api/auth/login')
